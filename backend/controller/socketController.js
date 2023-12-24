58bc8087274e55c84d4958ff60e06a715e01957d
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-12-24 18:03:53 +0530

    added collaborator route

--- backend/controller/socketController.js
+++ backend/controller/socketController.js
@@ -1,24 +1,32 @@
-import db from "../models/user";
+const connectDB = require("../models/connect");
 
-export const addCollaborator = async (req, res) => {
-  const { collaboratorId, docId } = req.body;
-  try {
-    // Assuming `doc` is your Mongoose model
-    const updatedDoc = await db.findOneAndUpdate(
-      { _id: docId },
-      { $push: { collaborators: collaboratorId } },
-      { new: true }
-    );
+const addCollaborator = async (req, res) =>
+{
+    const { collaboratorId, docId } = req.body;
+    try
+    {
+        // Assuming `doc` is your Mongoose model
+        const updatedDoc = await db.findOneAndUpdate(
+            { _id: docId },
+            { $push: { collaborators: collaboratorId } },
+            { new: true }
+        );
 
-    // Check if the document was updated successfully
-    if (!updatedDoc) {
-      return res.status(404).json({ message: "Document not found" });
+        // Check if the document was updated successfully
+        if (!updatedDoc)
+        {
+            return res.status(404).json({ message: "Document not found" });
+        }
+
+        // Respond with the updated document or a success message
+        res.status(200).json({ message: "Collaborator added successfully", updatedDoc });
+    } catch (error)
+    {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
+};
 
-    // Respond with the updated document or a success message
-    res.status(200).json({ message: "Collaborator added successfully", updatedDoc });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+module.exports = {
+    addCollaborator
 };
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createNote,addCollaborators,getNoteDetails,deleteNote,updateNote,getNotesByOwner } = require("../controller/noteController")
+const { createNote, addCollaborators, getNoteDetails, deleteNote, updateNote, getNotesByOwner } = require("../controller/noteController");
+const { addCollaborator } = require('../controller/socketController');
 
-router.post('/create-note/:id',createNote)
-router.post('/add-collbrators/:id',addCollaborators)
-router.get('/get-note-details/:id',getNoteDetails)
-router.delete('/delete-note/:id',deleteNote)
-router.put('/update-note/:id',updateNote)
-router.get('/get-my-notes/:id',getNotesByOwner)
+router.post('/create-note/:id', createNote);
+router.post('/add-collbrators/:id', addCollaborators);
+router.get('/get-note-details/:id', getNoteDetails);
+router.delete('/delete-note/:id', deleteNote);
+router.put('/update-note/:id', updateNote);
+router.get('/get-my-notes/:id', getNotesByOwner);
+router.post('/add-collaborator', addCollaborator);
 
 
 module.exports = router;
