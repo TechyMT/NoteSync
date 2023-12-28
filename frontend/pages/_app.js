fab857d9c2d5b72dd6070b7a2b471f4d818a2a05
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-12-28 21:19:05 +0530

    Delete unnecessary binary files

--- frontend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css';
-import { AuthProvider } from '@/utils/auth';
-
-export default function App({ Component, pageProps }) {
-  return <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>;
-}
