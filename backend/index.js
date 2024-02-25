fc577c1b71e4c4b0558f0d8ab5ca1b80add174db
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-02-25 13:02:18 +0530

    modified the error

--- backend/index.js
+++ backend/index.js
@@ -23,19 +23,19 @@ app.use("/api", userRouter);
 app.use("/api", notesRouter);
 app.use("", defaultRouter);
 
-
-cron.schedule("15 * * * *", () =>
+cron.schedule("*/15 * * * *", () =>
 {
-    console.log("Sending scheduled request at ", new Date());
+    console.log("Sending scheduled request at ", new Date().toLocaleDateString());
     request('https://hack-o-rama.onrender.com/ping', function (error, response, body)
     {
         if (!error && response.statusCode == 200)
         {
-            console.log('im ok');
-            // console.log(body) // Show the HTML for the Google homepage.
+            console.log("im okay");
+            // console.log(body) // Optionally, log the response body
         }
     });
 });
+
 
 const PORT = process.env.PORT || 8000;
 
